edb5fd4e63ebcee605519ef1003e2f8453a03c19
LouPInnovations <LouPInnovations@users.noreply.github.com>
2021-01-06 23:59:48 -0500

    Fixed bugs for Heroku Deployment

--- server.js
+++ server.js
@@ -62,9 +62,9 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
 }
 
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 //Listening on defined port
 app.listen(port, () => {
